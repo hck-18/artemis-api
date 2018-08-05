9ff8e13e46cd3d27deaf8e2348452bfcb48b66ea
Ariel Patiño <ariel@ficticio.com>
2018-08-04 23:44:18 -0500

    Add Establishment model

---
+++ api/models/Establishment.js
@@ -0,0 +1,20 @@
+/**
+ * Establishment.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name: { type: 'string', required: true },
+    lat: { type: 'number', required: true, columnType: 'float' }
+    lng: { type: 'number', required: true, columnType: 'float' },
+    address: { type: 'string', required: true },
+    cities_id: { type: 'number', required: true },
+    verified: { type: 'boolean', defaultsTo: true }
+  },
+
+};
+
